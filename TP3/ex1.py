c5aea3eaba65367bf68406aec2b1dcc672492839
aurelienamat <aurelien.amat@sfr.fr>
2026-04-27 17:43:58 +0200

    Python

---
+++ TP3/ex1.py
@@ -0,0 +1,5 @@
+val = int(input("Valeur : "))
+if val%2 != 0 :
+    print("inpaire")
+else :
+    print("Paire")
---
+++ TP3/ex2.py
@@ -0,0 +1,5 @@
+val = int(input("Val : "))
+somme = 0
+for i in range(val+1):
+    somme+=i
+print(somme)
