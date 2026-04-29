ed42dddbb05035f3c645b060aaf06d10e1f35c00
aurelienamat <aurelien.amat@sfr.fr>
2026-04-29 14:23:07 +0200

    oublie exo

--- TP3/ex1.js
+++ TP3/ex1.js
@@ -1,1 +1,20 @@
-document.body.innerHTML=""
+document.body.innerHTML = ""
+//création du formulaire
+const inputX = document.createElement('input');
+const boutonX = document.createElement('button');
+inputX.id = 'monInputX';
+// boutonX.value = 'Clic';
+// Ajoute les éléments au body
+document.body.appendChild(inputX);
+document.body.appendChild(boutonX);
+//ajout de l'évènement clic pour capter le moment ou le user valide le formulaire
+boutonX.onclick = () => {
+    const valeur = inputX.value;
+    console.log("Valeur entrée :", valeur);
+
+    if(parseInt(valeur)%2 > 0){
+        console.log("Impaire");
+    }else{
+        console.log("Paire");
+    }
+};
---
+++ TP3/ex2.js
@@ -0,0 +1,23 @@
+document.body.innerHTML = ""
+//création du formulaire
+const inputX = document.createElement('input');
+const boutonX = document.createElement('button');
+inputX.id = 'monInputX';
+// boutonX.value = 'Clic';
+// Ajoute les éléments au body
+document.body.appendChild(inputX);
+document.body.appendChild(boutonX);
+//ajout de l'évènement clic pour capter le moment ou le user valide le formulaire
+boutonX.onclick = () => {
+    const valeur = inputX.value;
+    console.log("Valeur entrée :", valeur);
+
+    let valFin = 0;
+
+    for (let i = 1; i <= parseInt(valeur); i++) {
+        valFin += i;
+    }
+
+    console.log(valFin);
+
+};
